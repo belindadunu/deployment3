cb337ced73e6ec844757b6f1965b0fba07194a69
Belinda <belindadunu24@gmail.com>
2023-09-22 15:26:28 +0000

    rollback to original version

--- test_app.py
+++ test_app.py
@@ -1,6 +1,6 @@
-from application import app, greet
+from application import app
 
-def test_quick():
-  a = "jeff"
-  greeting = greet(a)
-  assert greeting == "Hi jeff"
+
+def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
